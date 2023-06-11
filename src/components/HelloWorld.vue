39f859a8cfc128771551450e68e492d1bdafab98
pippi0057 <pippi0057@gmail.com>
2023-06-11 21:56:05 +0900

    add itemlist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,22 @@
 <script setup lang="ts">
+import ClickCounter from './ClickCounter.vue'
+import ItemList from './ItemList.vue'
+
 defineProps<{
   msg: string
 }>()
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div>
+    <h1>{{ msg }}</h1>
+    <ClickCounter />
+    <ItemList />
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+a {
+  color: #42b983;
 }
 </style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+    interface Item {
+        name: string
+        price: number
+    }
+
+    const items = ref<Item[]>([
+        { name: 'egg', price: 100},
+        { name: 'apple', price: 160}
+    ])
+
+    const newItemName = ref('')
+    const newItemPrice = ref(0)
+
+    const addItem = () => {
+        items.value.push({ name: newItemName.value, price: newItemPrice.value})
+        newItemName.value = ''
+        newItemPrice.value = 0
+    }
+</script>
+
+
+<template>
+    <div>ItemList</div>
+    <ul>
+        <li v-for="item in items" :key="item.name">
+            <div>name: {{ item.name }}</div>
+            <div>{{ item.price }} yen</div>
+        </li>
+    </ul>
+    <div>
+        <label>
+            name
+            <input v-model="newItemName" type="text" />
+        </label>
+        <label>
+            price
+            <input v-model="newItemPrice" type="number" />
+        </label>
+        <button @click="addItem">add</button>
+    </div>
+</template>
+
+<style></style>
